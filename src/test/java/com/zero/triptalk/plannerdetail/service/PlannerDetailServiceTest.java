216807e952c2412b828b5a840b24d7df215d3e71
postrel63 <postrel63@naver.com>
2023-09-24 06:10:17 +0900

    [feat] feature-createPlannerDetail 세부 일정 요청이 한 개만 들어왔을 때
    createPlannerDetail Service 테스트 코드 작성
    Mock 테스트
    맞는 boolean 값이 나오는지,
    저장된 place 값이 일치하는지 확인

--- src/test/java/com/zero/triptalk/plannerdetail/service/PlannerDetailServiceTest.java
+++ src/test/java/com/zero/triptalk/plannerdetail/service/PlannerDetailServiceTest.java
@@ -1,15 +1,33 @@
 package com.zero.triptalk.plannerdetail.service;
 
-import com.zero.triptalk.place.repository.PlaceRepository;
+import com.zero.triptalk.place.entity.Place;
+import com.zero.triptalk.place.entity.PlaceRequest;
 import com.zero.triptalk.place.service.ImageService;
 import com.zero.triptalk.place.service.PlaceService;
+import com.zero.triptalk.plannerdetail.dto.PlannerDetailRequest;
+import com.zero.triptalk.plannerdetail.entity.PlannerDetail;
+import com.zero.triptalk.plannerdetail.repository.PlannerDetailRepository;
+import com.zero.triptalk.user.entity.UserEntity;
+import com.zero.triptalk.user.enumType.UserTypeRole;
 import com.zero.triptalk.user.repository.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PlannerDetailServiceTest {
@@ -23,6 +41,9 @@ class PlannerDetailServiceTest {
     @Mock
     private ImageService imageService;
 
+    @Mock
+    private PlannerDetailRepository plannerDetailRepository;
+
     @InjectMocks
     private PlannerDetailService plannerDetailService;
 
@@ -31,12 +52,49 @@ class PlannerDetailServiceTest {
     void createPlannerDetail() {
 
         //given
+        Long planId = 1L;
+        String email = "test@exam.com";
+        List<MultipartFile> files = new ArrayList<>();
 
+        UserEntity user = UserEntity.builder()
+                .userId(1L)
+                .UserType(UserTypeRole.USER)
+                .nickname("11")
+                .email("test@exam.com")
+                .password("11")
+                .build();
+
+        Place place = Place.builder()
+                .id(1L)
+                .name("남산")
+                .region("한국")
+                .si("서울시")
+                .gun("서울군")
+                .gu("서울구")
+                .address("남산상세")
+                .latitude(10)
+                .longitude(10)
+                .build();
+
+        PlannerDetailRequest request = PlannerDetailRequest.builder()
+                .id(1L)
+                .date(LocalDateTime.now())
+                .description("테스트 요청입니다.")
+                .placeInfo(new PlaceRequest("남산", "한국", "서울시", "서울군", "서울구", "남산상세", 10, 10))
+                .build();
+
+        ArgumentCaptor<PlannerDetail> captor = ArgumentCaptor.forClass(PlannerDetail.class);
 
         //when
-
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        when(placeService.savePlace(any())).thenReturn(place);
+        boolean result = plannerDetailService.createPlannerDetail(planId, files, request, email);
 
         //then
+        verify(plannerDetailRepository).save(captor.capture());
+        PlannerDetail saved = captor.getValue();
 
+        Assertions.assertThat(result).isTrue();
+        Assertions.assertThat(saved.getPlace()).isEqualTo(place);
     }
 }
