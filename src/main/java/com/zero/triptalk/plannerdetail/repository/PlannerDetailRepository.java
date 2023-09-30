3571e7d74599cdcd68c4f685af411751048f2cc0
postrel63 <postrel63@naver.com>
2023-09-30 19:05:21 +0900

    feature: 해당 일정의 상세일정을 조회하기 위한 findByPlanner

--- src/main/java/com/zero/triptalk/plannerdetail/repository/PlannerDetailRepository.java
+++ src/main/java/com/zero/triptalk/plannerdetail/repository/PlannerDetailRepository.java
@@ -1,12 +1,14 @@
 package com.zero.triptalk.plannerdetail.repository;
 
-import com.zero.triptalk.plannerdetail.dto.PlannerDetailResponse;
+import com.zero.triptalk.plannerdetail.entity.Planner;
 import com.zero.triptalk.plannerdetail.entity.PlannerDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlannerDetailRepository extends JpaRepository<PlannerDetail, Long> {
 
-
+    List<PlannerDetail> findByPlanner(Planner planner);
 }
