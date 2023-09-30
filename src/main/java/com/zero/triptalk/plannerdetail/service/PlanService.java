61c21136f9235087e45f7b6ced7286b457e32917
postrel63 <postrel63@naver.com>
2023-09-30 16:04:36 +0900

    feature: planService.java 생성

---
+++ src/main/java/com/zero/triptalk/plannerdetail/service/PlanService.java
@@ -0,0 +1,12 @@
+package com.zero.triptalk.plannerdetail.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PlanService {
+
+
+
+}
