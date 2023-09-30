f31cabd48231b4a154d99f3a99413f9253bd4c96
postrel63 <postrel63@naver.com>
2023-09-30 22:47:44 +0900

    feature: application 패키지 구분
    controller 와 service 사이의 application 생성

---
+++ src/main/java/com/zero/triptalk/application/PlannerApplication.java
@@ -0,0 +1,10 @@
+package com.zero.triptalk.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PlannerApplication {
+
+}
