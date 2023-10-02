8fe8a6e4e95f109b4faef88bc45c3da85b5d8a7e
postrel63 <postrel63@naver.com>
2023-10-02 17:56:19 +0900

    feature: @NoArgsConstructor @AllArgsConstructor 추가

--- src/main/java/com/zero/triptalk/planner/dto/PlannerDetailListRequest.java
+++ src/main/java/com/zero/triptalk/planner/dto/PlannerDetailListRequest.java
@@ -1,12 +1,13 @@
 package com.zero.triptalk.planner.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zero.triptalk.place.entity.Place;
 import com.zero.triptalk.place.entity.PlaceRequest;
 import com.zero.triptalk.planner.entity.Planner;
 import com.zero.triptalk.planner.entity.PlannerDetail;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,8 @@ import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlannerDetailListRequest {
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime date;
