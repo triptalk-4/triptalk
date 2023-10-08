3b74c87d9ee409d49d43aaa3ffba8e1c8fed0511
postrel63 <postrel63@naver.com>
2023-10-09 02:46:24 +0900

    feature: 일정 조회 목록 서비스 생성

--- src/main/java/com/zero/triptalk/planner/service/PlannerService.java
+++ src/main/java/com/zero/triptalk/planner/service/PlannerService.java
@@ -1,30 +1,44 @@
 package com.zero.triptalk.planner.service;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.zero.triptalk.exception.code.PlannerErrorCode;
 import com.zero.triptalk.exception.custom.PlannerException;
+import com.zero.triptalk.planner.dto.PlannerListResponse;
 import com.zero.triptalk.planner.dto.PlannerRequest;
 import com.zero.triptalk.planner.entity.Planner;
+import com.zero.triptalk.planner.repository.CustomPlannerRepository;
 import com.zero.triptalk.planner.repository.PlannerRepository;
+import com.zero.triptalk.planner.type.SortType;
 import com.zero.triptalk.user.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PlannerService {
 
     private final PlannerRepository plannerRepository;
+    private final JPAQueryFactory queryFactory;
+    private final CustomPlannerRepository customPlannerRepository;
+
 
     public Planner createPlanner(PlannerRequest request, UserEntity user) {
-       return plannerRepository.save(request.toEntity(user));
+        return plannerRepository.save(request.toEntity(user));
     }
 
-    public Planner findById(Long plannerId){
+    public Planner findById(Long plannerId) {
         return plannerRepository.findById(plannerId).orElseThrow(
                 () -> new PlannerException(PlannerErrorCode.NOT_FOUND_PLANNER));
     }
 
     public void deletePlanner(Long plannerId) {
         plannerRepository.deleteById(plannerId);
+    }
+
+    public List<PlannerListResponse> getPlanners(Long lastId, int limit, SortType sortType) {
+        return customPlannerRepository.PlannerList(lastId, limit, sortType);
+
     }
 }
