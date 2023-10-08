2074d123d04907f2ff70e8dc4629752c1e5f854e
postrel63 <postrel63@naver.com>
2023-10-09 02:45:24 +0900

    feature: import 통일

--- src/main/java/com/zero/triptalk/like/entity/DetailPlannerLike.java
+++ src/main/java/com/zero/triptalk/like/entity/DetailPlannerLike.java
@@ -1,9 +1,6 @@
 package com.zero.triptalk.like.entity;
 
-import com.zero.triptalk.planner.dto.PlannerStatus;
-import com.zero.triptalk.planner.entity.Planner;
 import com.zero.triptalk.planner.entity.PlannerDetail;
-import com.zero.triptalk.user.entity.UserEntity;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
