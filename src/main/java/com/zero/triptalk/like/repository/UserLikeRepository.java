66a4b23b153a2766fb5abd2efc43c0a7c7f023df
anjaehun <juhun104@naver.com>
2023-10-13 16:01:27 +0900

    feat. 유저 좋아요 보기 게시글 완성

--- src/main/java/com/zero/triptalk/like/repository/UserLikeRepository.java
+++ src/main/java/com/zero/triptalk/like/repository/UserLikeRepository.java
@@ -4,8 +4,13 @@ import com.zero.triptalk.like.entity.UserLikeEntity;
 import com.zero.triptalk.planner.entity.Planner;
 import com.zero.triptalk.planner.entity.PlannerDetail;
 import com.zero.triptalk.user.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserLikeRepository extends JpaRepository<UserLikeEntity, Long> {
@@ -13,4 +18,16 @@ public interface UserLikeRepository extends JpaRepository<UserLikeEntity, Long> 
     boolean existsByPlannerDetailAndUser(PlannerDetail plannerDetail, UserEntity userEntity);
 
     Optional<Object> findByPlannerDetailAndUser(PlannerDetail plannerDetail, UserEntity user);
+
+    @Query("SELECT pl, COUNT(pl) as likeCount FROM PlannerLike pl WHERE pl.planner = :planner GROUP BY pl")
+    List<Object[]> findLikesCountByPlanner(@Param("planner") Planner planner);
+
+    // 다음과 같이 유저가 좋아요 한 플래너만을 가져오는 메서드를 정의합니다.
+    @Query("SELECT pl.planner, COUNT(pl) as likeCount " +
+            "FROM PlannerLike pl " +
+            "WHERE pl.planner.user = :user " +
+            "GROUP BY pl.planner")
+    Page<Object[]> findPlannersLikedByUserWithLikeCount(@Param("user") UserEntity user, Pageable pageable);
+
+
 }
