ea668ef386cbd931ba1840fda21299c22fb5e011
postrel63 <postrel63@naver.com>
2023-10-13 20:10:03 +0900

    refactor: 일정 생성 오류 메세지 UPDATE_PLANNER_FAILED

--- src/main/java/com/zero/triptalk/exception/code/PlannerErrorCode.java
+++ src/main/java/com/zero/triptalk/exception/code/PlannerErrorCode.java
@@ -10,7 +10,8 @@ public enum PlannerErrorCode {
 
 
     NOT_FOUND_PLANNER(HttpStatus.BAD_REQUEST, "일정이 존재하지 않습니다."),
-    UNMATCHED_USER_PLANNER(HttpStatus.BAD_REQUEST, "본인의 게시물만 수정/삭제할 수 있습니다.");
+    UNMATCHED_USER_PLANNER(HttpStatus.BAD_REQUEST, "본인의 게시물만 수정/삭제할 수 있습니다."),
+    UPDATE_PLANNER_FAILED(HttpStatus.BAD_REQUEST, "일정 업데이트 실패");
 
     private final HttpStatus status;
     private final String errorMessage;
