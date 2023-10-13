44c01677c70eebab6cb72fe81331bea31feb8467
anjaehun <juhun104@naver.com>
2023-10-13 16:02:01 +0900

    feat. 그외 설정

--- src/main/java/com/zero/triptalk/like/entity/PlannerLike.java
+++ src/main/java/com/zero/triptalk/like/entity/PlannerLike.java
@@ -1,6 +1,7 @@
 package com.zero.triptalk.like.entity;
 
 import com.zero.triptalk.planner.entity.Planner;
+import com.zero.triptalk.user.entity.UserEntity;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -22,6 +23,10 @@ public class PlannerLike {
     @ManyToOne
     @JoinColumn(name = "planner_id")
     private Planner planner;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
 
     private Long likeCount;
 
--- src/main/java/com/zero/triptalk/like/repository/PlannerLikeRepository.java
+++ src/main/java/com/zero/triptalk/like/repository/PlannerLikeRepository.java
@@ -3,11 +3,20 @@ package com.zero.triptalk.like.repository;
 import com.zero.triptalk.like.entity.PlannerLike;
 import com.zero.triptalk.planner.entity.Planner;
 import com.zero.triptalk.planner.entity.PlannerDetail;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PlannerLikeRepository extends JpaRepository<PlannerLike, Long> {
 
     PlannerLike findByPlanner(Planner planner);
 
     PlannerLike findByPlanner_PlannerId(Long plannerId);
+
+    @Query("SELECT pl, COUNT(pl) as likeCount FROM PlannerLike pl WHERE pl.planner = :planner GROUP BY pl")
+    Page<Object[]> findLikesCountByPlanner(@Param("planner") Planner planner, Pageable pageable);
+
+    Long countByPlanner(Planner planner);
 }
