7905792e264ebd04f29594eb961f7473e4cdb595
anjaehun <juhun104@naver.com>
2023-10-13 20:03:30 +0900

    feat. 유저 리펙터링

--- src/main/java/com/zero/triptalk/user/request/UpdateRegisterRequest.java
+++ src/main/java/com/zero/triptalk/user/request/UpdateRegisterRequest.java
@@ -9,8 +9,6 @@ import lombok.*;
 @NoArgsConstructor
 public class UpdateRegisterRequest {
     private String email;
-    private String oldPassword;
-    private String newPasswordCheck;
     private String newPassword;
     private String newNickname;
     private String newAboutMe;
