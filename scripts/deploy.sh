29e3a3c157c8ebd749f7ac2ec4c3df68d48f5d38
krystal-13 <honey9163@gmail.com>
2023-10-17 08:15:14 +0900

    feat. deploy.sh 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-REPOSITORY=/home/ec2-user/build
+REPOSITORY=/home/ec2-user/app/build/libs
 PROJECT_NAME=triptalk
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
+echo "> 현재 구동중인 애플리케이션 pid 확인" >> /home/ec2-user/app/deploy.log
 
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME)
 
-echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID" >> /home/ec2-user/app/deploy.log
 
 if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/app/deploy.log
 else
     echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
     sleep 5
 fi
 
-echo "> 새 어플리케이션 배포"
+echo "> 새 어플리케이션 배포" >> /home/ec2-user/app/deploy.log
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar)
 
 echo "> JAR Name: $JAR_NAME"
 
@@ -26,7 +26,7 @@ echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 실행"
+echo "> $JAR_NAME 실행" >> /home/ec2-user/app/deploy.log
 
 nohup java -jar \
     -Dspring.config.location=/home/ec2-user/config/application.yml \
