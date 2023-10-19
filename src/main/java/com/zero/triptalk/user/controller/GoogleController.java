b30a1a33c52a9c1cea2889c3d15722149e2cbe8d
Krystal-13 <129822965+Krystal-13@users.noreply.github.com>
2023-10-19 23:40:39 +0900

    Update GoogleController.java

--- src/main/java/com/zero/triptalk/user/controller/GoogleController.java
+++ src/main/java/com/zero/triptalk/user/controller/GoogleController.java
@@ -24,7 +24,7 @@ public class GoogleController {
     @GetMapping
     public ResponseEntity<AuthenticationResponse> loginGoogle(@RequestParam(value = "code") String code){
 
-        log.debug("controller :::::::::::::::" + code);
+        System.out.println("controller :::::::::  " + code);
 
         return ResponseEntity.ok(googleAuthService.doSocialLogin(code));
     }
