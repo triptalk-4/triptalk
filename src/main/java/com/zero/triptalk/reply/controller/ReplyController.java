926ae6ff388a1ddac47387e4aef66c52bafed542
anjaehun <juhun104@naver.com>
2023-10-19 14:43:29 +0900

    댓글 관련 처리 완료

--- src/main/java/com/zero/triptalk/reply/controller/ReplyController.java
+++ src/main/java/com/zero/triptalk/reply/controller/ReplyController.java
@@ -1,11 +1,15 @@
 package com.zero.triptalk.reply.controller;
 
 import com.zero.triptalk.reply.dto.request.ReplyRequest;
+import com.zero.triptalk.reply.dto.response.ReplyGetResponse;
 import com.zero.triptalk.reply.dto.response.ReplyResponse;
+import com.zero.triptalk.reply.entity.ReplyEntity;
 import com.zero.triptalk.reply.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,6 +17,17 @@ import org.springframework.web.bind.annotation.*;
 public class ReplyController {
 
     private final ReplyService replyService;
+
+
+    @GetMapping("/detail/replies/{plannerDetailId}")
+    @PreAuthorize("hasAuthority('USER')")
+    public List<ReplyGetResponse> getRepliesByPlannerDetail(@PathVariable Long plannerDetailId) {
+        // replyService를 사용하여 PlannerDetail 번호로 댓글을 조회
+        List<ReplyGetResponse> replyEntities = replyService.getRepliesByPlannerDetailId(plannerDetailId);
+
+        return replyEntities;
+    }
+
 
     /**
      * 댓글 하나 등록
