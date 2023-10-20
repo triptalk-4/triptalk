83ca7b5131b3e9dbe9e0b956e8d3c81e013d7b90
postrel63 <postrel63@naver.com>
2023-10-21 03:59:45 +0900

    feature: 쿼리를 통한 조회수 업데이트

--- src/main/java/com/zero/triptalk/planner/repository/PlannerRepository.java
+++ src/main/java/com/zero/triptalk/planner/repository/PlannerRepository.java
@@ -5,6 +5,7 @@ import com.zero.triptalk.user.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,6 +15,9 @@ public interface PlannerRepository extends JpaRepository<Planner, Long> {
     List<Planner> findByUser(UserEntity user);
     Page<Planner> findByUser(UserEntity user, Pageable pageable);
 
+    @Modifying
+    @Query("update Planner p set p.views = :view where p.plannerId = :plannerId")
+    void updateViews(@Param("view")Long view, @Param("plannerId") Long plannerId);
 
     @Query("SELECT p, COALESCE(pll.likeCount, 0) as likeCount\n" +
             "FROM Planner p\n" +
