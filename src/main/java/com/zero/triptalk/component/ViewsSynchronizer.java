2bad978f2b06a19c7e548cdc2ec9d24bc3f31c0b
postrel63 <postrel63@naver.com>
2023-10-21 14:57:04 +0900

    feature: keys -> scan 방식으로 변경

--- src/main/java/com/zero/triptalk/component/ViewsSynchronizer.java
+++ src/main/java/com/zero/triptalk/component/ViewsSynchronizer.java
@@ -3,6 +3,8 @@ package com.zero.triptalk.component;
 import com.zero.triptalk.planner.entity.Planner;
 import com.zero.triptalk.planner.repository.PlannerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -24,8 +26,6 @@ public class ViewsSynchronizer {
     /**
      * look aside + write back
      * 조회수 동기화
-     *
-     *
      **/
     @Transactional
 //    @Scheduled(cron = "0 0/3 * * * *")
@@ -40,10 +40,10 @@ public class ViewsSynchronizer {
                 continue;
             } else {
                 Planner planner = optionalPlanner.get();
-                assert view != null;
-                planner.setViews(Long.valueOf(view));
-                plannerRepository.save(planner);
-
+                if (view != null) {
+                    planner.setViews(Long.valueOf(view));
+                    plannerRepository.save(planner);
+                }
                 entityManager.flush();
                 entityManager.clear();
             }
@@ -53,13 +53,24 @@ public class ViewsSynchronizer {
     @Transactional
     @Scheduled(cron = "0 0/3 * * * *")
     public void updateDBFromRedisBySQL() {
-        Set<String> keys = stringRedisTemplate.keys("planner:views:*");
-        assert keys != null;
-        for (String key : keys) {
-            Long plannerId = Long.parseLong(key.split(":")[2]);
-            String view = stringRedisTemplate.opsForValue().get(key);
-            assert view != null;
-            plannerRepository.updateViews(Long.valueOf(view), plannerId);
+
+        ScanOptions options = ScanOptions.scanOptions()
+                .match("planner:views:*")
+                .count(10)
+                .build();
+        Cursor<byte[]> cursor = stringRedisTemplate.executeWithStickyConnection(
+                connection -> connection.scan(options)
+        );
+        if (cursor != null) {
+            while (cursor.hasNext()) {
+                String key = new String(cursor.next());
+                Long plannerId = Long.parseLong(key.split(":")[2]);
+                String view = stringRedisTemplate.opsForValue().get(key);
+                if (view != null) {
+                    plannerRepository.updateViews(Long.valueOf(view), plannerId);
+                }
+            }
         }
     }
+
 }
