6d57a244cd8d1f82f07001165dc72d5d7e1e5ef7
postrel63 <postrel63@naver.com>
2023-10-25 15:52:29 +0900

    feature: checkDuplication 조회수 중복 체크 메서드, getAdd 으로 중복사용되는 로직을 분리함
    조회수 중복 체크는 자정까지 만료시간 설정
    increaseViewsUser는 사용하지 않지만 나중에 캐시를 통해 사용

--- src/main/java/com/zero/triptalk/planner/service/PlannerService.java
+++ src/main/java/com/zero/triptalk/planner/service/PlannerService.java
@@ -17,6 +17,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -50,47 +53,45 @@ public class PlannerService {
         return customPlannerRepository.PlannerList(pageable, sortType);
     }
 
+    private void getAdd(Long plannerId, String key) {
+        stringRedisTemplate.opsForSet().add(key, String.valueOf(plannerId));
+    }
 
-    /**
-     * plannerId을 key 로 하여 방문한 사용자들을 set 으로 저장
-     *
-     */
+    public void increaseViews(Long plannerId) {
+        plannerRepository.increaseViews(plannerId);
+    }
 
-    public void increaseViews(Planner planner, Long userId) {
-        String user = String.valueOf(userId);
-        String key = "plannerId:" + planner.getPlannerId();
-        String totalViewsKey = "planner:views:" + planner.getPlannerId();
-        // set에 key가 존재하지 않으면(최근 24시간 내에 조회된 적이 없으면)
+    public Boolean checkDuplication(Long plannerId, Long loginUserId) {
+        String key = "user:" + loginUserId;
         if (Boolean.FALSE.equals(stringRedisTemplate.hasKey(key))) {
-            stringRedisTemplate.opsForSet().add(key, user);
-            stringRedisTemplate.expire(key, 24, TimeUnit.HOURS);
-            stringRedisTemplate.opsForValue().set(totalViewsKey, String.valueOf(planner.getViews() + 1), 4L, TimeUnit.MINUTES);
-            //레디스에 이미 올라가있음, 거기에 set에 아이디가 없으면 아이디 추가, 조회수 1 증가, 만료시간 설정
-        } else if (Boolean.FALSE.equals(stringRedisTemplate.opsForSet().isMember(key, user))) {
-            stringRedisTemplate.opsForSet().add(key, user);
-            if (Boolean.FALSE.equals(stringRedisTemplate.hasKey(totalViewsKey))) {
-                stringRedisTemplate.opsForValue().set(totalViewsKey, String.valueOf(planner.getViews()));
-            }
-            stringRedisTemplate.opsForValue().increment(totalViewsKey);
-            stringRedisTemplate.expire(totalViewsKey, 4L, TimeUnit.MINUTES);
+            getAdd(plannerId, key);
+            LocalDateTime now = LocalDateTime.now();
+            LocalDateTime midnight = LocalDateTime.of(now.toLocalDate().plusDays(1), LocalTime.MIDNIGHT);
+            long secondsUntilMidnight = Duration.between(now, midnight).toSeconds();
+            stringRedisTemplate.expire(key, secondsUntilMidnight, TimeUnit.SECONDS);
+            return false;
         }
+        if (Boolean.FALSE.equals(stringRedisTemplate.opsForSet().isMember(key, String.valueOf(plannerId)))) {
+            getAdd(plannerId, key);
+            return false;
+        }
+        return true;
     }
 
     /**
-     * userId를 key 로 하여 방문한 페이지를 set 으로 저장
-     *
+     * userId를 key 로 하여 방문한 페이지를 set 으로 저장 , 조회수도 캐시로 저장
      */
     public void increaseViewsUser(Long views, Long plannerId, Long loginUserId) {
         String key = "user:" + loginUserId;
         String totalView = "planner:views:" + plannerId;
 
         if (Boolean.FALSE.equals(stringRedisTemplate.hasKey(key))) {
-            stringRedisTemplate.opsForSet().add(key, String.valueOf(plannerId));
+            getAdd(plannerId, key);
             stringRedisTemplate.expire(key, 24, TimeUnit.HOURS);
             stringRedisTemplate.opsForValue().set(totalView, String.valueOf(views + 1), 4L, TimeUnit.MINUTES);
             //user set 이 있고 조회된 적이 없으면
         } else if (Boolean.FALSE.equals(stringRedisTemplate.opsForSet().isMember(key, String.valueOf(plannerId)))) {
-            stringRedisTemplate.opsForSet().add(key, String.valueOf(plannerId));
+            getAdd(plannerId, key);
             if (Boolean.FALSE.equals(stringRedisTemplate.hasKey(totalView))) {
                 stringRedisTemplate.opsForValue().set(totalView, String.valueOf(views));
             }
