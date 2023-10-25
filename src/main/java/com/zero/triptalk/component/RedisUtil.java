59b1495a69616075125af00feab28164c5fe8ed3
postrel63 <postrel63@naver.com>
2023-10-26 00:38:53 +0900

    feature: 기존 stringRedisTemplate으로 직접 처리하던 Redis 코드를 RedisUtil에 모두 올림
    isMember:          set에 게시글이 존재하는지
    hasKey:            set key가 존재하는지
    addUserSet:        set에 게시글 추가
    increaseViews:     조회수 1 증가
    setExpireMidnight: 자정까지 만료시간

--- src/main/java/com/zero/triptalk/component/RedisUtil.java
+++ src/main/java/com/zero/triptalk/component/RedisUtil.java
@@ -1,11 +1,15 @@
 package com.zero.triptalk.component;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -15,13 +19,13 @@ public class RedisUtil {
 
 
     public String getData(String key) {
-        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
         return valueOperations.get(key);
     }
 
 
     public void setData(String key, String value) {
-        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
         valueOperations.set(key, value);
     }
 
@@ -34,5 +38,34 @@ public class RedisUtil {
     public void deleteData(String key) {
         stringRedisTemplate.delete(key);
     }
+
+    // 조회수
+
+    public Boolean isMember(String key, String plannerId) {
+        SetOperations<String, String> setOps = stringRedisTemplate.opsForSet();
+        return Boolean.TRUE.equals(setOps.isMember(key, plannerId));
+    }
+
+    public Boolean hasKey(String key) {
+        return stringRedisTemplate.hasKey(key);
+    }
+
+    public void addUserSet(String key, Long plannerId) {
+        SetOperations<String, String> setOps = stringRedisTemplate.opsForSet();
+        setOps.add(key, String.valueOf(plannerId));
+    }
+
+    public void increaseViews(String plannerKey) {
+        ValueOperations<String, String> valueOps = stringRedisTemplate.opsForValue();
+        valueOps.increment(plannerKey);
+    }
+
+    public void setExpireMidnight(String key) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime midnight = LocalDateTime.of(now.toLocalDate().plusDays(1), LocalTime.MIDNIGHT);
+        long secondsUntilMidnight = Duration.between(now, midnight).toSeconds();
+        stringRedisTemplate.expire(key, secondsUntilMidnight, TimeUnit.SECONDS);
+    }
+
 
 }
